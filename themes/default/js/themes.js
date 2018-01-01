01882a04ee7089a5337aca9fff591f03518eee0e
patrikx3 <alabard@gmail.com>
2018-01-01 14:14:24 +0100

    p3x-robot sunday release 2018-1-1 14:14:24

--- themes/default/js/themes.js
+++ themes/default/js/themes.js
@@ -2,8 +2,8 @@
 var themes = {
     "bootstrap-default": "/themes/default/css/bootstrap-default.css",
     "bootstrap-cerulean": "/themes/default/css/bootstrap-cerulean.css",
-    "bootstrap-cosmo": "/themes/default/css/bootstrap-cosmo.css",
     "bootstrap-cyborg": "/themes/default/css/bootstrap-cyborg.css",
+    "bootstrap-cosmo": "/themes/default/css/bootstrap-cosmo.css",
     "bootstrap-darkly": "/themes/default/css/bootstrap-darkly.css",
     "bootstrap-flatly": "/themes/default/css/bootstrap-flatly.css",
     "bootstrap-journal": "/themes/default/css/bootstrap-journal.css",
@@ -12,9 +12,9 @@ var themes = {
     "bootstrap-readable": "/themes/default/css/bootstrap-readable.css",
     "bootstrap-sandstone": "/themes/default/css/bootstrap-sandstone.css",
     "bootstrap-simplex": "/themes/default/css/bootstrap-simplex.css",
+    "bootstrap-superhero": "/themes/default/css/bootstrap-superhero.css",
     "bootstrap-slate": "/themes/default/css/bootstrap-slate.css",
     "bootstrap-spacelab": "/themes/default/css/bootstrap-spacelab.css",
-    "bootstrap-superhero": "/themes/default/css/bootstrap-superhero.css",
     "bootstrap-united": "/themes/default/css/bootstrap-united.css",
     "bootstrap-yeti": "/themes/default/css/bootstrap-yeti.css"
 }
